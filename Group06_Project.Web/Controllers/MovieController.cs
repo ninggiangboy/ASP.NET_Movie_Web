ea63905a0fb97ac8623119e747f9c9728b1a1128
AnhKhuong10 <anhkhuong989@gmail.com>
2024-05-29 13:25:50 +0700

    addView movieDetail

---
+++ Group06_Project.Web/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+
+namespace Group06_Project.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly ILogger<MovieController> _logger;
+
+        public MovieController(ILogger<MovieController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int Id)
+        {
+            _logger.LogInformation(Id.ToString());
+            return View();
+        }
+    }
+}
