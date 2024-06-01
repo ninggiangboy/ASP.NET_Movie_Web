262ad170986d3cea15d97c73384d91feb7a55bee
aransumaru <107389800+aransumaru@users.noreply.github.com>
2024-06-01 16:54:06 +0700

    task

--- Group06_Project.Domain/Interfaces/Services/IFilmService.cs
+++ Group06_Project.Domain/Interfaces/Services/IFilmService.cs
@@ -1,9 +1,11 @@
+using Group06_Project.Domain.Models;
+
 namespace Group06_Project.Domain.Interfaces.Services;
 
 public interface IFilmService
 {
-    void GetLatestFilm();
-    void GetPopularFilm();
-    void GetFeatureFilm();
+    Page<FilmHomeModel> GetLatestFilm(int page, int size);
+    Page<FilmHomeModel> GetPopularFilm(int page, int size);
+    Page<FilmHomeModel> GetFeatureFilm(int page, int size);
     void GetFilmDetail();
 }
